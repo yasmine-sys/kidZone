24b06d68b8a2002a5f0eeb6ea5441bebc2d55700
khaoula-RAJHI <khaoula.rajhi@esprit.tn>
2022-05-10 20:46:51 +0100

    STRIPE API

---
+++ src/main/java/tn/pi/spring/controller/PaymentRestController.java
@@ -0,0 +1,31 @@
+package tn.pi.spring.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stripe.model.Charge;
+
+import tn.pi.spring.service.StripeClient;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentRestController {
+
+    private StripeClient stripeClient;
+
+    @Autowired
+    PaymentRestController(StripeClient stripeClient) {
+        this.stripeClient = stripeClient;
+    }
+
+    @PostMapping("/charge")
+    public Charge chargeCard(HttpServletRequest request) throws Exception {
+        String token = request.getHeader("token");
+        Double amount = Double.parseDouble(request.getHeader("amount"));
+        return this.stripeClient.chargeCreditCard(token, amount);
+    }
+}
